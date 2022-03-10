ef9408ed2b0e58c6541ece01f21e175b8bf48c61
madina-dauletqyzy <madina.iitu@gmail.com>
2022-03-10 17:39:48 +0500

    add mapping data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-
+import notes from "../notes";
 export default function App() {
   return (
     <div>
   <Header />
-  <Note />
+  {notes.map(props=>(
+    <Note 
+      title = {props.title}
+      content = {props.content}
+    />
+  ))}
+ 
   <Footer />
   </div>);
 }
